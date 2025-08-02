cd289861ba0aa7c302b901914a3e6784fe5e33cb
Tushar More <tmore9534@gmail.com>
2025-08-02 15:26:11 +0530

    Added the user and token entities

--- app/src/main/java/authservice/entities/userRole.java
+++ app/src/main/java/authservice/entities/userRole.java
@@ -1,5 +1,29 @@
 package authservice.entities;
 
-public class userRole {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String name;
 
 }
